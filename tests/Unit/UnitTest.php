7bfe9fb40d00086bf9f69d43acd067e2f0247507
tunmin11 <tunmin2000@gmail.com>
2020-01-13 14:04:18 +0630

    cicd

---
+++ tests/Unit/UnitTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UnitTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertEquals(2,1+1);
+    }
+}
